cfd5057f229bc7c476e1ad0f4be345473b0da153
DE BARROS Geoffrey <debarrosg01@gmail.com>
2023-06-13 18:09:17 +0200

    Test mobile first home

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,78 +3,75 @@ import React from "react";
 const Cards = () => {
   return (
     <div>
-    <div className="card">
-      <div className="title">
-        <h3>Titre de la BA</h3>
+      <div className="card">
+        <div className="title">
+          <h3>Titre de la BA</h3>
+        </div>
+        <div className="info-ba">
+          <span>B.A de : Sacha</span>
+          <span>06/06/2023</span>
+        </div>
+        <div className="text-ba">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+            dolorem non est facere ratione sed eligendi expedita explicabo saepe
+            similique obcaecati accusantium assumenda, maxime suscipit pariatur
+            quam iste consequuntur animi a. Repudiandae assumenda sint earum
+            exercitationem voluptatum, provident explicabo sed.
+          </p>
+        </div>
+        <div className="likes">
+          <div className="like-count">8 likes</div>
+          <button>&#x2764;</button>
+        </div>
       </div>
-      <div className="info-ba">
-        <span>B.A de : Sacha</span>
-        <span>06/06/2023</span>
+
+      <div className="card">
+        <div className="title">
+          <h3>Titre de la BA</h3>
+        </div>
+        <div className="info-ba">
+          <span>B.A de : Sacha</span>
+          <span>06/06/2023</span>
+        </div>
+        <div className="text-ba">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+            dolorem non est facere ratione sed eligendi expedita explicabo saepe
+            similique obcaecati accusantium assumenda, maxime suscipit pariatur
+            quam iste consequuntur animi a. Repudiandae assumenda sint earum
+            exercitationem voluptatum, provident explicabo sed.
+          </p>
+        </div>
+        <div className="likes">
+          <div className="like-count">8 likes</div>
+          <button>&#x2764;</button>
+        </div>
       </div>
-      <div className="text-ba">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-          dolorem non est facere ratione sed eligendi expedita explicabo saepe
-          similique obcaecati accusantium assumenda, maxime suscipit pariatur
-          quam iste consequuntur animi a. Repudiandae assumenda sint earum
-          exercitationem voluptatum, provident explicabo sed.
-        </p>
-      </div>
-      <div className="likes">
-        <div className="like-count">8 likes</div>
-        <button>&#x2764;</button>
+
+      <div className="card">
+        <div className="title">
+          <h3>Titre de la BA</h3>
+        </div>
+        <div className="info-ba">
+          <span>B.A de : Sacha</span>
+          <span>06/06/2023</span>
+        </div>
+        <div className="text-ba">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+            dolorem non est facere ratione sed eligendi expedita explicabo saepe
+            similique obcaecati accusantium assumenda, maxime suscipit pariatur
+            quam iste consequuntur animi a. Repudiandae assumenda sint earum
+            exercitationem voluptatum, provident explicabo sed.
+          </p>
+        </div>
+        <div className="likes">
+          <div className="like-count">8 likes</div>
+          <button>&#x2764;</button>
+        </div>
       </div>
     </div>
-
-    <div className="card">
-      <div className="title">
-        <h3>Titre de la BA</h3>
-      </div>
-      <div className="info-ba">
-        <span>B.A de : Sacha</span>
-        <span>06/06/2023</span>
-      </div>
-      <div className="text-ba">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-          dolorem non est facere ratione sed eligendi expedita explicabo saepe
-          similique obcaecati accusantium assumenda, maxime suscipit pariatur
-          quam iste consequuntur animi a. Repudiandae assumenda sint earum
-          exercitationem voluptatum, provident explicabo sed.
-        </p>
-      </div>
-      <div className="likes">
-        <div className="like-count">8 likes</div>
-        <button>&#x2764;</button>
-      </div>
-    </div>
-
-    <div className="card">
-      <div className="title">
-        <h3>Titre de la BA</h3>
-      </div>
-      <div className="info-ba">
-        <span>B.A de : Sacha</span>
-        <span>06/06/2023</span>
-      </div>
-      <div className="text-ba">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-          dolorem non est facere ratione sed eligendi expedita explicabo saepe
-          similique obcaecati accusantium assumenda, maxime suscipit pariatur
-          quam iste consequuntur animi a. Repudiandae assumenda sint earum
-          exercitationem voluptatum, provident explicabo sed.
-        </p>
-      </div>
-      <div className="likes">
-        <div className="like-count">8 likes</div>
-        <button>&#x2764;</button>
-      </div>
-    </div>
-
-    </div>
-
-    
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Footer from "./Footer";
 
 const Header = () => {
   const [isNavigationOpen, setIsNavigationOpen] = useState(false);
