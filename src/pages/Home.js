ef09833e80d4c2defffd97c780bbc5476bc8b929
DE BARROS Geoffrey <debarrosg01@gmail.com>
2023-06-13 16:23:15 +0200

    Home style ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Cards from '../components/Cards';
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <Cards />
         </div>
     );
 };
