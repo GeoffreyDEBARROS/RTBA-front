b53dea44212b283898068b061316dee1d4bc34e9
DE BARROS Geoffrey <debarrosg01@gmail.com>
2023-06-14 12:23:38 +0200

    Call api messages home ok

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,76 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment/moment";
+
 
 const Cards = () => {
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/messages")
+      .then((res) => setMessages(res.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div>
-      <div className="card">
-        <div className="title">
-          <h3>Titre de la BA</h3>
+      {messages.map((item, index) => (
+        <div className="card" key={index}>
+          <div className="title">
+            <h3>{item.title}</h3>
+          </div>
+          <div className="info-ba">
+            <span>B.A de : {item.user_id}</span>
+            <span>{moment(item.created_at).format('DD/MM/YY')}</span>
+          </div>
+          <div className="text-ba">
+            <p>{item.content}</p>
+          </div>
+          <div className="likes">
+            <div className="like-count">8 likes</div>
+            <button>&#x2764;</button>
+          </div>
         </div>
-        <div className="info-ba">
-          <span>B.A de : Sacha</span>
-          <span>06/06/2023</span>
-        </div>
-        <div className="text-ba">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-            dolorem non est facere ratione sed eligendi expedita explicabo saepe
-            similique obcaecati accusantium assumenda, maxime suscipit pariatur
-            quam iste consequuntur animi a. Repudiandae assumenda sint earum
-            exercitationem voluptatum, provident explicabo sed.
-          </p>
-        </div>
-        <div className="likes">
-          <div className="like-count">8 likes</div>
-          <button>&#x2764;</button>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="title">
-          <h3>Titre de la BA</h3>
-        </div>
-        <div className="info-ba">
-          <span>B.A de : Sacha</span>
-          <span>06/06/2023</span>
-        </div>
-        <div className="text-ba">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-            dolorem non est facere ratione sed eligendi expedita explicabo saepe
-            similique obcaecati accusantium assumenda, maxime suscipit pariatur
-            quam iste consequuntur animi a. Repudiandae assumenda sint earum
-            exercitationem voluptatum, provident explicabo sed.
-          </p>
-        </div>
-        <div className="likes">
-          <div className="like-count">8 likes</div>
-          <button>&#x2764;</button>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="title">
-          <h3>Titre de la BA</h3>
-        </div>
-        <div className="info-ba">
-          <span>B.A de : Sacha</span>
-          <span>06/06/2023</span>
-        </div>
-        <div className="text-ba">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-            dolorem non est facere ratione sed eligendi expedita explicabo saepe
-            similique obcaecati accusantium assumenda, maxime suscipit pariatur
-            quam iste consequuntur animi a. Repudiandae assumenda sint earum
-            exercitationem voluptatum, provident explicabo sed.
-          </p>
-        </div>
-        <div className="likes">
-          <div className="like-count">8 likes</div>
-          <button>&#x2764;</button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
