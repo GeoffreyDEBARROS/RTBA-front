6f09129a081d01d6ff5ef915bae2a17dcb91113d
DE BARROS Geoffrey <debarrosg01@gmail.com>
2023-06-17 22:08:23 +0200

    supp de lignes inutiles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
